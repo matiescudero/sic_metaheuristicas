7dfa440d74c2616b56af40ecf59a99d66d26b564
matias.escudero.b <matias.escudero.b@usach.cl>
2022-10-22 23:43:51 -0300

    shapes de salida

--- DATOS/R/sic_model.R
+++ DATOS/R/sic_model.R
@@ -29,12 +29,14 @@ connect_to_db = function(dvr, db, host, port, user, passwd){
 
 # Almacenar tablas espaciales
 
-postgis_to_df = function(connection, layer){
+postgis_to__rownames(dij_matrix, "zc_id")
+  dij_matrix = as.matrix(dij_matrix)
+  return(dij_matrix)df = function(connection, layer){
   df = st_read(connection, layer)
-  return(df)
 }
 
-# Transformar dataframe de distancias a matriz
+# Transformar dataframe de distancias a matriz  dij_matrix = column_to
+
 
 df_to_matrix = function(distance_df){
   dij_matrix = spread(distance_df, stop_id, distance)
@@ -113,7 +115,7 @@ swap<-function(sol,i,j){
 evaluate_SIC = function(instancia, xj){
 #' Evalua la interacción espacial entre un nodo de demanda i (zona censal) y un paradero j.
 #' 
-#' @param instancia (list) Lista que in
+#' @param instancia (list) Lista que incluye la matriz dij y los vectores ai, ni y wj
 #' @param xj (array) Vector de solución que indica que paraderos j son localizados y cuales no
 #' 
 #' @return acum (int) Valor de interacción espacial entre el nodo i y el paradero j
@@ -204,38 +206,137 @@ calculate_initial_temperature = function(instancia, xj, spatial_interaction, p0)
   return(t_inicial)
 }
 
+simulated_annealing = function(instancia, xj_ini, n_iter, alpha){
+  #' Calcula el menor costo al aplicar el algoritmo de S.A a una función objetivo dada.
+  #' 
+  #' @param instancia (list) Lista que incluye la matriz dij y los vectores ai, ni y wj
+  #' @param xj_ini (array) Vector que contiene una configuración inicial de Xj para el modelo SIC
+  #' @param n_iter (int) Número de iteraciones máximas a realizar hasta que el algoritmo no entregue una mejor solución que la actual
+  #' @param alpha (float) Factor de enfriamiento de la temperatura por cada nuevo ciclo
+  #' 
+  #' @return xj (array) mejor configuración de Xj encontrada
+  #' @return spatial_interaction (float) máximo valor de interacción espacial encontrada para el modelo SIC
+  #' @return eval_si (array) solución actual para cada iteración
+  #' @return eval_mejor (array) mejor solución encontrada para cada iteración
+  #' @return temp (array) evolución de la temperatura para cada iteración
+  #'   
+  ## Variables de tracking
+  eval_si = numeric()
+  eval_mejor = numeric()
+  temp = numeric()
+  
+  # Inicialización soluciones (xj)
+  ## Se genera el vector de solución inicial 
+  xj = xj_ini
+  mejor_xj = xj
+  
+  ## Se obtiene un primer interacción espacial a partir de la evaluación de una solución random
+  spatial_interaction = evaluate_SIC(instancia, xj)
+  mejor_si = spatial_interaction
+  
+  ## Se obtiene la temperatura inicial
+  t_inicial = calculate_initial_temperature(instancia, xj, spatial_interaction, 0.5)
+  t = t_inicial
+  
+  ## Contador para ciclo loop
+  i = 1
+  n = length(xj)
+  
+  ## Ciclo while
+  while(i < n_iter){
+    
+    #otro wail
+    
+    n_swap = get_swap_numbers(xj)
+    xj_test = swap(xj, n_swap[1], n_swap[2])
+    si_test = evaluate_SIC(instancia, xj_test)
+    
+    # Se chequea si se reemplaza la solución
+    delta_si = spatial_interaction - si_test
+    
+    if(exp(-(delta_si)/t) > runif(1)){ 
+      
+      xj = xj_test
+      spatial_interaction = si_test
+    }
+    
+    # Se actualiza la mejor solución
+    if(si_test >= mejor_si){
+      mejor_xj = xj_test
+      mejor_si = si_test
+      # Si la solución es la mejor hasta el momento, se reinicia el contador
+      i = 1
+    }else{
+      # Si la solución del iterador es peor, se suma uno al contador y se pasa a la sig iteración,
+      # El ciclo termina una vez que al iterar n_iter veces la solución no mejore.
+      i = i + 1
+    }
+    
+    # Variables de trackeo
+    eval_si = c(eval_si, spatial_interaction)
+    eval_mejor = c(eval_mejor, mejor_si)
+    temp = c(temp, t)
+    
+    ##Equilibrium state?
+    
+    # Se aplica un factor de enfriamiento de alpha (parámetro de la función)
+    t = t*alpha
+  }
+  
+  return(list(xj = mejor_xj, 
+              spatial_interaction = mejor_si, 
+              eval_si=eval_si, 
+              eval_mejor=eval_mejor,
+              temp = temp))
+}
 
-# Generar conexión
+
+
+#### MAIN ####
+
+### ENTRADAS ###
+
+## Conexión a BD
 
 con = connect_to_db(dvr, db, host, port, user, passwd)
 
-# ENTRADAS
+## Tablas de entrada
 
 nodos_demanda = postgis_to_df(con, "nodos_demanda")
 paraderos = postgis_to_df(con, "paraderos")
 dij = postgis_to_df(con, "dij")
 
-# PROCESAMIENTO
+### PROCESAMIENTO ###
 
-# DF a Matriz
+## Dataframe dij a matriz
 
 dij_matrix = df_to_matrix(dij)
-
-# Se transforman las columnas a vectores
 
 ## Se agrupan las entradas de interés en una única instancia
 instancia = make_instance(dij_matrix, nodos_demanda$ai, nodos_demanda$ni, paraderos$wj)
 
-# SIMULATED ANNEALING
-
 ## Se genera la solución inicial
-xj = generate_initial_solution(paraderos, 90)
+xj_ini = generate_initial_solution(paraderos, 90)
+
+### Resultados ####
+
+resultados_sa = simulated_annealing(instancia, xj_ini, 200, 0.5)
+
+resultados$spatial_interaction
 
 
-## Evaluación SIC
-spatial_interaction = evaluate_SIC(instancia, xj)
+plot(resultados$eval_si, type = "l", col = "red", 
+     main = "Simulated Annealing\n Operador SWAP",
+     xlab = "N° iteración",
+     ylab = "Spatial Interaction")
 
-## 
-initial_t = calculate_initial_temperature(instancia, xj, spatial_interaction, 0.5)
 
+
+## Nuevos resultados
+paraderos$xj = resultados$xj
+
+
+#output
+st_write(obj = paraderos, 
+         "DATOS/SHP/paraderos_sa.shp")
 
