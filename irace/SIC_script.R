db0533232836a33302e6ab1d8f10a7c87cb31c75
matias.escudero.b <matias.escudero.b@usach.cl>
2022-12-17 19:51:52 -0300

    modificación funcion DatToInstance

--- irace/SIC_script.R
+++ irace/SIC_script.R
@@ -2,7 +2,7 @@
 #### FUNCIONES #####
 ####################
 
-DatToInstance = function(nombre_archivo){
+DatToInstance = function(nombre_archivo, n_nodos, n_paraderos){
   #' Lee un archivo DAT y extrae los vectores y los almacena en una lista como instancia.
   #' 
   #' @param nombre_archivo (character) Nombre del archivo de extensión .DAT
@@ -15,29 +15,29 @@ DatToInstance = function(nombre_archivo){
   contenido_dat = read.delim(nombre_archivo, header=FALSE, sep =" ")
   
   # Se extrae la matriz
-  dij = contenido_dat[(2:527),(2:100)]
+  dij = contenido_dat[(2:(n_nodos + 1)),(2:(n_paraderos + 1))]
   dij = as.matrix(dij)
   class(dij) = "numeric"
-  colnames(dij) = contenido_dat[1,1:99]
-  rownames(dij) = contenido_dat[2:527,1]
+  colnames(dij) = contenido_dat[1,1:n_paraderos]
+  rownames(dij) = contenido_dat[2:(n_nodos + 1),1]
   
   # Se elimina la matriz del contenido dat
-  contenido_dat = contenido_dat[-c(1:528),]
+  contenido_dat = contenido_dat[-c(1:(n_nodos + 2)),]
   
   # Se extrae el vector ai
-  ai = as.numeric(contenido_dat[(1:526),2])
+  ai = as.numeric(contenido_dat[(1:n_nodos),2])
   
   # Se elimina el vector ai
-  contenido_dat = contenido_dat[-c(1:527),]
+  contenido_dat = contenido_dat[-c(1:(n_nodos + 1)),]
   
   # Se extrae el vector ni
-  ni = as.numeric(contenido_dat[(1:526),2])
+  ni = as.numeric(contenido_dat[(1:n_nodos),2])
   
   # Se elimina el vector ni
-  contenido_dat = contenido_dat[-c(1:527),]
+  contenido_dat = contenido_dat[-c(1:n_nodos + 1),]
   
   # Se extrae el vector wj
-  wj = as.numeric(contenido_dat[(1:99),2])
+  wj = as.numeric(contenido_dat[(1:n_paraderos),2])
   
   # Se almacenan los vectores en una lista
   instancia = list(dij = dij, ai = ai, ni = ni, wj = wj)
@@ -47,19 +47,19 @@ DatToInstance = function(nombre_archivo){
 # Generar solución inicial aleatoria
 
 GenerateInitialSolution = function(instancia, p){
-  #' Genera el vector Xj inicial de forma aleatoria, indicando el n° p de paraderos a seleccionar.
+  #' Genera el vector Xj inicial de forma aleatoria, indicando el n° p de paraderos a eliminar.
   #' Además, se incluye el nombre de cada paradero en el vector generado. 
   #'
   #' @param instancia (list) Instancia que contiene dij, ai, ni y wj. 
-  #' @param p (int) Número de paraderos a localizar. Debe ser igual o menor a n.
+  #' @param p (int) Número de paraderos a eliminar. Debe ser igual o menor a n.
   #'
   #' @return xj (array) Vector de solución generado aleatoriamente
   
   # Número de paraderos disponibles
-  n = length(instancia$wj)
+  n = length(instancia$wj) 
   
   # Vector de solución aleatoria
-  xj = c(rep(0, n - p), rep(1, p))
+  xj = c(rep(0, p), rep(1, n - p))
   xj = sample(xj)
   
   # Se indica el id de cada paradero para el vector generado
@@ -72,10 +72,10 @@ GenerateInitialSolution = function(instancia, p){
 
 ## Operadores ----
 
-
 GetSwapNumbers = function(xj){
-#' Obtiene las posiciones para realizar el operador swap. Si al evaluar estas posiciones dentro del
-#' vector xj son idénticas se obtendrán nuevos números hasta que dejen de ser iguales.
+  
+  #' Obtiene las posiciones para realizar el operador swap. Si al evaluar estas posiciones dentro del
+  #' vector xj son idénticas se obtendrán nuevos números hasta que dejen de ser iguales.
   
   n = length(xj)
   is_equal = TRUE
@@ -92,14 +92,13 @@ GetSwapNumbers = function(xj){
   return(n_swap)
 }
 
-Swap<-function(sol,i,j){
+Swap = function(sol,i,j){
   
   piv<-sol[i]
   sol[i]<-sol[j]
   sol[j]<-piv
   return(sol)
 }
-
 
 SplitSolution = function(xj){
   #' Separa un vector de solución en un vector de ceros y otro de unos.
@@ -117,8 +116,10 @@ SplitSolution = function(xj){
   return(xj_list)
 }
 
-
 SwapSplit = function(xj_list){
+  
+  print("xj_list:")
+  print(xj_list)
   
   xj_zeros = xj_list$zeros
   xj_ones = xj_list$ones
@@ -135,9 +136,92 @@ SwapSplit = function(xj_list){
   
   # Pasar listas solo a una y ordenarla
   xj = c(xj_list$zeros, xj_list$ones)
+  
+  print("xj:")
+  print(xj)
+  
   xj = xj[order(names(xj))]
   
   return(xj)
+  
+}
+
+TwoPointCrossover = function(parent1, parent2){
+  #' Genera un cruzamiento de dos puntos entre dos padres y devuelve dos hijos.
+  
+  # largo del vector de solución
+  n = length(parent1)
+  
+  ## 
+  is_equal_sum = TRUE
+  
+  while (is_equal_sum){
+    
+    # Se eligen dos números entre 1 y n
+    points = sort(sample(1:n, 2))
+    
+    ## se generan los vectores a cruzar
+    piv1 = parent1[points[1]:points[2]]
+    piv2 = parent2[points[1]:points[2]]
+    
+    
+    ## Se chequea si es que la suma de los vectores es igual, sino se ejecuta de nuevo 
+    if (sum(piv1) == sum(piv2)){
+      
+      parent1[points[1]:points[2]] = piv2
+      parent2[points[1]:points[2]] = piv1
+      
+      is_equal_sum = FALSE
+    } 
+  }
+  
+  children = parent1
+  
+  return(children)
+}
+
+CrossoverManuel = function(padre1, padre2){
+  #' Almacena las posciones de los ceros de los vectores padres, extrae al azar p ceros y genera un hijo.
+  #' 
+  #' @parameter padre1 (list): Vector de solución al problema de optimización que se cruza con el padre 2
+  #' @parameter padre2 (list): Vector de solución al problema de optimización que se cruza con el padre 1
+  #' 
+  #' @return hijo (list): Vector resultante del cruzamiento entre los dos padres
+  
+  # Largo del vector
+  len_padre = length(padre1)
+  
+  # Se genera una lista con unos
+  hijo = padre1
+  hijo[1:len_padre] = 1
+  
+  
+  # Se almacenan las posiciones de los ceros de ambos vectores
+  ceros_p1 = which(padre1 == 0)
+  ceros_p2 = which(padre2 == 0)
+  
+  # Se almacena la cantidad de ceros de los vectores
+  n_ceros = length(ceros_p1)
+  
+  # Se unen las posiciones 
+  cero_padres = c(ceros_p1, ceros_p2)
+  
+  # Se eliminan los duplicados
+  lista_ceros = unique(cero_padres)
+  
+  
+  names(lista_ceros) = unique(names(cero_padres))
+  
+  # Se alojan los nombres de los paraderos que son ceros en los padres
+  paraderos_cero = names(lista_ceros)
+  
+  # Se extraen al azar n_ceros paraderos de la lista de nombre de paraderos
+  sample_paraderos = sample(paraderos_cero, n_ceros)
+  
+  # Se reemplazan los ceros en el hijo
+  hijo[sample_paraderos] = 0
+  
+  return(hijo)
   
 }
 
@@ -175,6 +259,41 @@ EvaluateSIC = function(instancia, xj){
   return(acum)
 }
 
+EvaluatePopulationSIC = function(instancia, poblacion){
+  #' Calcula el SIC para cada elemento de una población y la probabilidad de selección de cada miembro
+  #' 
+  #' @param instancia (list) Lista que incluye la matriz dij y los vectores ai, ni y wj
+  #' @param poblacion (matrix) matriz que contiene que contiene n vectores de solución
+  #' 
+  #' @return sic_poblacion (array) vector que contiene el SIC de cada uno de los miembros de la población
+  #' @return prob_seleccion (array) Probabilidad de selección de cada miembro de la población
+  #' 
+  
+  ## Número de miembros de la población
+  n_miembros = ncol(poblacion)
+  
+  ## Se evalúa el SIC para cada vector de solución de la población dada
+  sic_poblacion = apply(poblacion, 2, function(x) EvaluateSIC(instancia, x))
+  
+  ## Se almacena el mínimo y máximo SIC obtenido
+  max_sic = max(sic_poblacion)
+  min_sic = min(sic_poblacion)
+  
+  ## Se calcula la probabilidad de selección para cada miembro de la población
+  if (max_sic == min_sic){
+    
+    ## Si es que los valores de SIC son iguales, la probabilidad de selección es la misma para todos.
+    prob_seleccion = rep(1/n_miembros, n_miembros)
+  } else {
+    
+    ## Si los valores de SIC son distintos, los que sean mejores tendrán una mayor probabilidad de selección
+    suma_sic = sum(sic_poblacion)
+    prob_seleccion = sic_poblacion/suma_sic
+  }
+  
+  return(list(sic_poblacion = sic_poblacion,
+              prob_seleccion = prob_seleccion))
+}
 
 CalculateInitialTemperature = function(instancia, xj, spatial_interaction, p0){
   
@@ -224,7 +343,6 @@ CalculateInitialTemperature = function(instancia, xj, spatial_interaction, p0){
   return(t_inicial)
 }
 
-
 CalculateInitialTemperature2 = function(instancia, xj, spatial_interaction, p0){
   
   
@@ -253,6 +371,7 @@ CalculateInitialTemperature2 = function(instancia, xj, spatial_interaction, p0){
 }
 
 
+## Metaheurísticas ----
 
 SimulatedAnnealing = function(instancia, xj_ini, operador, max_iter, max_iter_interna, alpha){
   #' Calcula el menor costo al aplicar el algoritmo de S.A a una función objetivo dada.
@@ -367,19 +486,105 @@ SimulatedAnnealing = function(instancia, xj_ini, operador, max_iter, max_iter_in
   
   eval_time = c(eval_time, time_iter)
   
-  print(paste("El mejor Resultado encontrado es:", mejor_si))
-  
-  resultados_sa = list(xj = mejor_xj, 
-                       spatial_interaction = mejor_si, 
-                       eval_si=eval_si, 
-                       eval_mejor=eval_mejor,
-                       temp = temp,
-                       eval_iter = eval_iter,
-                       eval_time = eval_time,
-                       eval_si_iter = eval_si_iter)
+  return(list(xj = mejor_xj, 
+              spatial_interaction = mejor_si, 
+              eval_si=eval_si, 
+              eval_mejor=eval_mejor,
+              temp = temp,
+              eval_iter = eval_iter,
+              eval_time = eval_time,
+              eval_si_iter = eval_si_iter))
+}
 
-  print(paste("El mejor Resultado encontrado es:", resultados_sa$spatial_interaction))
+GeneticAlgorithm = function(instancia, n_miembros, operador, n_paraderos, max_iter, prob_mutacion){
   
-  return(mejor_si)
+  
+  ## Tamaño de la solución del problema
+  n = length(instancia$wj)
+  
+  # Evolución del mejor sic
+  evol = numeric(max_iter)
+  
+  ## Se genera la población con la cantidad n_miembros 
+  padres = replicate(n_miembros, GenerateInitialSolution(instancia, n_paraderos))
+  
+  ## Se inicializa la matriz que almacenará a los hijos
+  hijos = replicate(n_miembros, numeric(n))
+  
+  # Se le asignan los nombres de los paraderos a los hijos
+  rownames(hijos) = rownames(padres)
+  
+  ## Se inicializan las variables que almacenan el mejor sic y mejor vector de solución
+  best_sic = -Inf
+  best_sol = numeric(n)
+  
+  ## Ciclo que se repetirá max_iter veces
+  
+  iter = 1
+  
+  while (iter <= max_iter) {
+    
+    print(paste("Iteración número:", iter))
+    
+    ## Se evalúa el SIC para todos los miembros de la población
+    evaluacion_sic = EvaluatePopulationSIC(instancia, padres)
+    
+    ## Se almacena el máximo valor encontrado para la iteración
+    sic_iter = max(evaluacion_sic$sic_poblacion)
+    
+    ## Se almacena la posición del mejor resultado encontrado
+    pos_max = which(evaluacion_sic$sic_poblacion == sic_iter)
+    
+    ## vector de mejor solución encontrada en el ciclo
+    sol_iter = padres[,pos_max]
+    
+    # REEMPLAZO DE SOLUCIÓN
+    
+    # Se obtiene la posición de la peor solución y se cambia por la mejor
+    pos_min = which(evaluacion_sic$sic_poblacion == min(evaluacion_sic$sic_poblacion))
+    hijos[,pos_min] = best_sol
+    
+    # Se actualiza la mejor solución y mejor fitness
+    if (sic_iter > best_sic){
+      
+      best_sol = sol_iter
+      best_sic = sic_iter
+    }
+    
+    ## Ciclo para generar hijos
+    for (miembro in 1:n_miembros){
+      
+      ## Se seleccionan los padres en base a su probabilidad de selección
+      n_padres = sample(1:n_miembros, 2, prob = evaluacion_sic$prob_seleccion)
+      padre1 = padres[,n_padres[1]]
+      padre2 = padres[,n_padres[2]]
+      
+      #Se genera un hijo mediante el cruzamiento de los padres
+      
+      if (operador == "two_point_crossover"){
+        
+        hijos[,miembro] = TwoPointCrossover(padre1, padre2)
+        
+      } else {
+        
+        hijos[,miembro] = CrossoverManuel(padre1, padre2)
+        
+      }
+      
+      #Se aplica un swap al hijo dada una probabilidad
+      n_swap = GetSwapNumbers(hijos[,miembro])
+      hijos[,miembro] = Swap(hijos[,miembro], n_swap[1], n_swap[2])
+    }
+    
+    # Los padres se convierten en hijos
+    padres = hijos
+    
+    # Se almacena el mejor SIC
+    evol[iter] = best_sic
+    iter = iter + 1
+  }
+  
+  return(list(sol = best_sol, sic = best_sic, evol = evol))
+  
 }
 
